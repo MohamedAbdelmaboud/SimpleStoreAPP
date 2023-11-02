6a4f348736ae1b26493ce746bc12cba1bf1f7c18
Mohamed Abdelmaboud <mohamedabdelmabod03@gmail.com>
2023-11-02 16:21:46 +0200

    .

--- lib/widgets/custom_field.dart
+++ lib/widgets/custom_field.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class CustomField extends StatelessWidget {
-  const CustomField({
+   CustomField({
     super.key,
     this.hintText,
     this.onChanged,
@@ -13,17 +14,21 @@ class CustomField extends StatelessWidget {
   final void Function(String)? onChanged;
   final TextEditingController? controller;
   final TextInputType? keyboardType;
+  final Pattern deniedPattern=RegExp(r'[\u0600-\u06FF\s]+');
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 10.0),
       child: TextField(
+        inputFormatters: [
+         // LengthLimitingTextInputFormatter(5), length for input
+          FilteringTextInputFormatter(deniedPattern, allow: false)
+          ],
         keyboardType: keyboardType,
         style: GoogleFonts.montserrat(color: Colors.black),
         onChanged: onChanged,
         decoration: InputDecoration(
             fillColor: Colors.white,
-            filled: true,
             hintText: hintText,
             hintStyle:
                 GoogleFonts.montserrat(color: Colors.black, fontSize: 14),
